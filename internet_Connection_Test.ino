de600a3159ba124cd9ab11350ada22e892b318a7
Beau Clark <SESA696863@se.com>
2023-02-21 07:07:25 -0500

    added 5 min reboot timer

--- internet_Connection_Test.ino
+++ internet_Connection_Test.ino
@@ -1,8 +1,7 @@
  /*
   Things to do 
-  1. Create 10 min timer to allow the router time to reboot and come back online before checking again.
-  2. put all the timers into there own dedicated functions for reuse if needed
-  3. Replace Serial out puts with LCD screen outputs after LCD is added. 
+  1. put all the timers into there own dedicated functions for reuse if needed
+  2. Replace Serial out puts with LCD screen outputs after LCD is added. 
 */
 
 #include <SPI.h>
@@ -16,15 +15,19 @@ EthernetClient client;
 
 unsigned long time_passed_0;
 unsigned long time_passed_1;
+unsigned long time_passed_2;
 unsigned long prev_time_0 = 0;
 unsigned long prev_time_1 = 0;
+unsigned long prev_time_2 = 0;
 unsigned long time_interval = 10000; //1800000; // 30 mins
 unsigned long power_off_time = 60000; // 1 min
+unsigned long wait_time = 300000; //5 mins
 
 bool connection_status = true;
 
 int relay_pin = 3;
-int count = 0;
+int count_1 = 0;
+int count_2 = 0;
 
 void setup() 
 {
@@ -46,35 +49,49 @@ void loop()
  Ethernet.maintain();
  time_passed_0 = millis();
 
+//Check the internet connection every 30 mins
  if(time_passed_0 - prev_time_0 >= time_interval)
  {
    prev_time_0 = time_passed_0;
    connection_status = connection_test();
- }
+  }
 
+//If connectio lost, start router reset procedure
  if(!connection_status)
  {
    Serial.println("Connection lost");
    connection_status = true;
    digitalWrite(relay_pin, HIGH);
 
-   while(count <= 1)
+  //Power down and reset the router
+   while(count_1 <= 1)
    {
      time_passed_1 = millis();
 
      if(time_passed_1 - prev_time_1 >= power_off_time)
      {
        prev_time_1 = time_passed_1;
-       count += 1;
-       Serial.println("Inside the IF statment: "); // For debuging
-     }
-     
-   }
+       count_1 += 1;
+      }
+    }
 
+  //Power the router back on
    digitalWrite(relay_pin, LOW);
-   count = 0;
- }
- 
+   count_1 = 0;
+   
+  //Wait for the router to boot back up
+   while(count_2 <= 1)
+   {
+     if(time_passed_2 - prev_time_2 >= wait_time)
+     {
+       prev_time_2 = time_passed_2;
+       count_2 += 1;
+      }
+    }
+
+    count_2 = 0;
+
+  }   
 }
 
 bool connection_test()
